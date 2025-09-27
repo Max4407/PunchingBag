ffa89b11b2b272252c1431fea9d396919fe62cbd
lehbob <lehbob2341@gmail.com>
2025-09-27 17:05:00 -0400

    Commiting before pull request

--- punchingbag-frontend/src/App.jsx
+++ punchingbag-frontend/src/App.jsx
@@ -1,11 +1,9 @@
 import { useState } from 'react'
 import './App.css'
+
 import UserTextBox from './components/UserTextBox'
-<<<<<<< HEAD
 import StressTracker from './components/graph'
-=======
 import Bag from './components/Bag'
->>>>>>> 069b55020c75da147951c12d45e16b8110a41dbe
 
 function App() {
   const [count, setCount] = useState(0)
