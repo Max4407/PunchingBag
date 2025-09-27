44629f42da3ebd0203067ee223e516ffded4652f
kNeff9 <kevinneff007@gmail.com>
2025-09-27 16:17:32 -0400

    Added ai response

--- punchingbag-frontend/src/App.jsx
+++ punchingbag-frontend/src/App.jsx
@@ -1,30 +1,38 @@
 import { useState } from 'react'
 import './App.css'
 import UserTextBox from './components/UserTextBox'
+import AIResponse from "./components/AIResponse"
 
 function App() {
   const [count, setCount] = useState(0)
+  const [aiResponse, setAiResponse] = useState(null)
 
   const [messages, setMessages] = useState([])
 
-  const handleSendMessage = (message) => {
 
-    setMessages((prev) => [...prev, message])
+  const handleSendMessage = async (message) => {
+    try {
+      const res = await fetch("http://localhost:5000/analyze", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({message})
+      })
+
+      const data = await res.json()
+      setAiResponse(data.response)
+    } catch (err) {
+      console.error("Backend Error:", err)
+    }
   }
 
   return (
     <>
       <div className='app'>
         <h1>Punch Me Bag</h1>
-        <div className='messages'>
-          {messages.map((msg, i) => (
-            <div key={i} className='message'>
-              {msg}
-            </div>
-          ))}
-        </div>
+        <h1>AI Message Analyzer</h1>
         <div style={{padding: 24}}>
           <UserTextBox onSend={handleSendMessage}/>
+          {aiResponse && <AIResponse response={aiResponse} />}
         </div>
       </div>
     </>
